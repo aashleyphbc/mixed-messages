39d54bfc8ceb1b6d06e81edb8a542c722a8906ec
aashleyphbc <170535690+aashleyphbc@users.noreply.github.com>
2024-06-11 16:45:11 -0400

    Addition: assign appropriate article (a/an) based on first letter of adjective

--- madlibs.js
+++ madlibs.js
@@ -1,6 +1,7 @@
 const nouns = ['bird', 'sandwich', 'brick', 'couch', 'gorilla', 'car', 'airplane', 'dog', 'chicken', 'computer'];
 const verbs = ['run', 'fly', 'swim', 'walk', 'crawl', 'sashay', 'creep', 'wander'];
-const adjectives = ['wild', 'beautiful', 'crazy', 'giant', 'tiny', 'hideous', 'angry', 'horrible', 'elite', 'oversized'];
+const adjectives = ['wild', 'beautiful', 'crazy', 'giant', 'tiny', 'hideous', 'angry', 'horrible', 'elite', 'oversized', 'orange', 'incoherent', 'unusual'];
+const vowels = ['a', 'e', 'i', 'o', 'u'];
 
 const getRandomNumber = num => {
     return Math.floor(Math.random() * num);
@@ -25,7 +26,11 @@ const generateMadlib = () => {
     let noun = getRandomNoun();
     let verb = getRandomVerb();
     let adjective = getRandomAdjective();
-    return `LOOK OUT! A ${adjective} ${noun} is about to ${verb} by!`;
+    if (vowels.includes(adjective[0])) {
+        return `LOOK OUT! An ${adjective} ${noun} is about to ${verb} by!`;
+    } else {
+        return `LOOK OUT! A ${adjective} ${noun} is about to ${verb} by!`;
+    }
 }
 
 console.log(generateMadlib());
